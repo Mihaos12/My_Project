eacbd903b91f16572c18643cb370c90ae7717686
Mihaos <123902992+Mihaos12@users.noreply.github.com>
2024-02-17 07:52:54 +0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+document.querySelector(".category-sviter").onclick = () => {
+  window.scrollTo(pageYOffset, 2000);
+};
+document.querySelector(".category-shapka").onclick = () => {
+  window.scrollTo(pageYOffset, 3900);
+};
+document.querySelector(".category-pinetka").onclick = () => {
+  window.scrollTo(pageYOffset, 5750);
+};
+document.querySelector(".category-pled").onclick = () => {
+  window.scrollTo(pageYOffset, 6750);
+};
+document.querySelector(".category-kombinezon").onclick = () => {
+  window.scrollTo(pageYOffset, 7750);
+};
